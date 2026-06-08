ff1ee0b9c120637f82f2e9b528f1dd6ffb636a7a
magarduno <m_angel1106@hotmail.com>
2026-06-07 20:33:58 -0600

    Update quiniela_2026.py

--- quiniela_2026.py
+++ quiniela_2026.py
@@ -737,7 +737,7 @@ if not st.session_state.user:
                     except Exception as e: st.error(f"Error: {e}")
                     finally: conn.close()
         elif "Cambiar" in opcion:
-            st.markdown("#### 🔓 Recuperar contraseña")
+            st.markdown("#### 🔓 C. Password")
             mensaje = st.empty()
             mensaje.warning("⚠️ Para realizar el cambio de contraseña se debe solicitar la autorización del administrador. "
                            "Si ya lo solicitaste, puedes realizar el cambio en esta sección.")
